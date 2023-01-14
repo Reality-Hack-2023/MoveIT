157ec41d64da5b48e0dab073bc9d85d8cb54bc93
Jack English <51383165+AUT0M4T1C-JACK@users.noreply.github.com>
2023-01-13 21:38:11 -0500

    Added Squirell Throwing Minigame
    Also removed unnecessary files

---
+++ MoveIT/Assets/Scripts/AcornSpawner.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcornSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject acorn;
+    [SerializeField] private float time = 0.0f;
+    [SerializeField] private float timeInterval = 2.0f;
+    public int acornsFell = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time >= timeInterval)
+        {
+            time = 0.0f;
+            Bounds bounds = GetComponent<Collider>().bounds;
+            float offsetX = Random.Range(-1.0f, 1.0f);
+            float offsetY = Random.Range(-0.25f, 0.25f);
+            float offsetZ = 0.0f;
+
+            GameObject acornObject = GameObject.Instantiate(acorn);
+            var controller = acornObject.GetComponent<AcornController>();
+            controller.acornSpawner = this;
+            acornObject.transform.position = bounds.center + new Vector3(offsetX, offsetY, offsetZ);
+
+        }
+    }
+}
